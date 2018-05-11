cd30fb9199661e32091c36fc22eb1e557c6899c8
Code Spot <39196039+abarna-codespot@users.noreply.github.com>
2018-05-11 21:01:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
